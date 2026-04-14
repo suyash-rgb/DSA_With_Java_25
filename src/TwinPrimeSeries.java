e3b15abf3f2ef096a7098842106bb8e9fc0b15e4
Suyash Baoney <suyashbaoney58@gmail.com>
2026-04-14 17:57:24 +0530

    twin prime

---
+++ src/TwinPrimeSeries.java
@@ -0,0 +1,28 @@
+// Twin Prime - Pairs
+// n1 n2  n2-n1=2
+// 3, 5
+
+boolean isPrime(int n) {
+    if (n <= 1)
+        return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void main() {
+    int limit = Integer.parseInt(IO.readln("Enter the no of pairs that you want: "));
+
+    int count = 0;
+    int num = 3;
+
+    while (count < limit) {
+        if (isPrime(num) && isPrime(num + 2)) {
+            count++;
+            IO.print("(" + num + ", " + (num + 2) + "), ");
+        }
+        num++;
+    }
+}
