50f5e30a7541c25c6d6ddf8f12c019d4f9d4f0d2
Suyash Baoney <suyashbaoney58@gmail.com>
2026-04-14 16:41:20 +0530

    Print a series of perfect numbers

---
+++ src/PerfectNumberSeries.java
@@ -0,0 +1,24 @@
+// Perfect Number - a number that is equal to the sum of proper divisors/factor
+// 6 - 1, 2, 3 
+
+void main() {
+    int n = Integer.parseInt(IO.readln("Enter the number of terms: "));
+
+    int count = 0;
+    int num = 1;
+
+    while (count < n) {
+        int sum = 0;
+        for (int j = 1; j <= num / 2; j++) { // mid-theorem
+            if (num % j == 0) {
+                sum += j;
+            }
+        }
+        if (sum == num && num != 0) {
+            IO.print(num + " ");
+            count++;
+        }
+        num++;
+    }
+
+}
