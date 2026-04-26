c11b907de29df19ce710551ca58582b2324a169a
Suyash Baoney <suyashbaoney58@gmail.com>
2026-04-27 00:51:58 +0530

    Square Pattern

---
+++ src/SquarePattern.java
@@ -0,0 +1,10 @@
+void main() {
+    int n = Integer.parseInt(IO.readln("Enter the size of the matrix: "));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            IO.print("* ");
+        }
+        IO.println();
+    }
+}
